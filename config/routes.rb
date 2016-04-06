66f8f5b9607b96c1986b64d06a616906a8b3f191
mumu <mumu@mumudeMacBook-Pro.local>
2016-04-06 21:11:28 +0900

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,44 @@
 Myapp::Application.routes.draw do
 
-  get '/recruits/show_recruits', to: 'recruits#show_recruits'
-  resources :recruits
+  # [my] catch ja/newsinfos/
+  scope ":locale", locale: /ja|en|de/ do
+    get '/recruits/show_recruits', to: 'recruits#show_recruits'
+    resources :recruits
 
-  get '/business_descriptions/show_business_descriptions', to: 'business_descriptions#show_business_descriptions'
-  resources :business_descriptions
+    get '/business_descriptions/show_business_descriptions', to: 'business_descriptions#show_business_descriptions'
+    resources :business_descriptions
 
-  resources :users
+    resources :users
 
-  get '/business_results/show_business_results', to: 'business_results#show_business_results'
-  resources :business_results
+    get '/business_results/show_business_results', to: 'business_results#show_business_results'
+    resources :business_results
 
-  resources :contacts
+    resources :contacts
 
-  get '/newsinfos/show_newsinfos', to: 'newsinfos#show_newsinfos'
-  resources :newsinfos
+    get '/newsinfos/show_newsinfos', to: 'newsinfos#show_newsinfos'
+    resources :newsinfos
 
-  get '/login/index', to: 'login#index'
-  get '/login/login/:id', to: 'login#login'
-  get '/login/my_page', to: 'login#my_page'
-  get '/login/logout', to: 'login#logout'
+    get '/login/index', to: 'login#index'
+    get '/login/login/:id', to: 'login#login'
+    get '/login/my_page', to: 'login#my_page'
+    get '/login/logout', to: 'login#logout'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+    # The priority is based upon order of creation: first created -> highest priority.
+    # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+    # You can have the root of your site routed with "root"
+    root 'welcome#index'
 
-  match ':controller(/:action(/:id))', via: [:get, :post, :patch]
+    match ':controller(/:action(/:id))', via: [:get, :post, :patch]
+
+  end
+
+  # [my] catch
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}") , via: [:get, :post, :patch]
+  match '', to: redirect("/#{I18n.default_locale}") , via: [:get, :post, :patch]
+
+
+
 
 
 
